5bf676887eb8fd5f23b9419dd19786af99750f12
Dustin Doss <ddoss@mit.edu>
2016-12-05 18:41:58 -0500

    Lab 6 Exercise 4

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,9 +1,14 @@
 #include <kern/e1000.h>
+#include <kern/pmap.h>
 
 // LAB 6: Your driver code here
+
+volatile uint32_t *e1000addr;
 
 int attach_E1000(struct pci_func *pcif)
 {
     pci_func_enable(pcif);
+    e1000addr = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+    uint32_t dev_status = *(e1000addr+2);
     return 0;
 }
