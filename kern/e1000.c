9b6f12d930abff0f40663c37feb5f09cab41e278
Dustin Doss <ddoss@mit.edu>
2016-12-05 20:53:35 -0500

    lab 6 exercise 5

--- kern/e1000.c
+++ kern/e1000.c
@@ -3,12 +3,18 @@
 
 // LAB 6: Your driver code here
 
-volatile uint32_t *e1000addr;
+volatile void *e1000addr;
+struct e1000_tx_desc txd_arr[E1000_TXDARR_LEN];
 
 int attach_E1000(struct pci_func *pcif)
 {
     pci_func_enable(pcif);
     e1000addr = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
-    uint32_t dev_status = *(e1000addr+2);
+    *(uint32_t *)(e1000addr+E1000_TDBAL) = (uint32_t)&txd_arr; // Indicates start of descriptor ring buffer
+    *((uint32_t *)e1000addr+E1000_TDLEN) = sizeof(struct e1000_tx_desc)*E1000_TXDARR_LEN; // Indicates start of descriptor ring buffer
+    *(uint32_t *)(e1000addr+E1000_TDH) = 0;
+    *(uint32_t *)(e1000addr+E1000_TDT) = 0;
+    *(uint32_t *)(e1000addr+E1000_TCTL) = (E1000_TCTL_EN | E1000_TCTL_PSP | E1000_TCTL_CT | E1000_TCTL_COLD);
+    *(uint16_t *)(e1000addr+E1000_TIPG) = (uint16_t)(E1000_TIPG_IPGT | E1000_TIPG_IPGR1 | E1000_TIPG_IPGR2);
     return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -4,4 +4,51 @@
 
 int attach_E1000(struct pci_func *pcif);
 
+/* Transmit Descriptor: sourced from e1000_hw.h */
+struct e1000_tx_desc {
+    uint64_t buffer_addr;       /* Address of the descriptor's data buffer */
+    uint16_t length;    /* Data buffer length */
+    uint8_t cso;        /* Checksum offset */
+    uint8_t cmd;        /* Descriptor control */
+    uint8_t status;     /* Descriptor status */
+    uint8_t css;        /* Checksum start */
+    uint16_t special;
+};
+
+#define E1000_TXDARR_LEN     32 /* Length of the transmit descriptor ring */
+/* Transmit Descriptor bit definitions */
+#define E1000_TXD_CMD_DEXT   0x20000000 /* Descriptor extension (0 = legacy) */
+
+/* Register Set. (82543, 82544)
+ *
+ * Registers are defined to be 32 bits and  should be accessed as 32 bit values.
+ * These registers are physically located on the NIC, but are mapped into the
+ * host memory address space.
+ *
+ * RW - register is both readable and writable
+ * RO - register is read only
+ * WO - register is write only
+ * R/clr - register is read only and is cleared when read
+ * A - register array
+ */
+
+#define E1000_TCTL     0x00400  /* TX Control - RW */
+#define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
+#define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
+
+/* Transmit Control */
+#define E1000_TCTL_EN     0x00000002    /* enable tx */
+#define E1000_TCTL_PSP    0x00000008    /* pad short packets */
+#define E1000_TCTL_CT     0x00000100    /* collision threshold */
+#define E1000_TCTL_COLD   0x00040000    /* collision threshold */
+
+/* Transmit IPG Register */
+#define E1000_TIPG_IPGT   10
+#define E1000_TIPG_IPGR1  8<<10
+#define E1000_TIPG_IPGR2  6<<20
+
+
 #endif	// JOS_KERN_E1000_H
