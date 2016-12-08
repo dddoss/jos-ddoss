420455547f0d0e7a3058f54f4ae1565c6379512d
Dustin Doss <ddoss@mit.edu>
2016-12-08 01:45:37 -0500

    Lab 6 Exercise 6

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,20 +1,73 @@
 #include <kern/e1000.h>
 #include <kern/pmap.h>
+#include <inc/string.h>
 
 // LAB 6: Your driver code here
 
 volatile void *e1000addr;
 struct e1000_tx_desc txd_arr[E1000_TXDARR_LEN];
+packet_t txd_bufs[E1000_TXDARR_LEN];
 
+/* Reset the TXD array entry corresponding to the given
+ * index such that it may be resused for another packet.
+ */
+void _reset_tdr(int index)
+{
+    txd_arr[index].buffer_addr = (uint32_t)(PADDR(txd_bufs+index));
+    txd_arr[index].cso = 0;
+    txd_arr[index].cmd = E1000_TXD_CMD_RS | E1000_TXD_CMD_EOP;
+    txd_arr[index].status = 0;
+    txd_arr[index].css = 0;
+    txd_arr[index].special = 0;
+}
 int attach_E1000(struct pci_func *pcif)
 {
     pci_func_enable(pcif);
     e1000addr = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
-    *(uint32_t *)(e1000addr+E1000_TDBAL) = (uint32_t)&txd_arr; // Indicates start of descriptor ring buffer
-    *((uint32_t *)e1000addr+E1000_TDLEN) = sizeof(struct e1000_tx_desc)*E1000_TXDARR_LEN; // Indicates start of descriptor ring buffer
+
+
+    // Initialize MMIO Region
+    // Manual section 14.5 initialization
+    *(uint32_t *)(e1000addr+E1000_TDBAL) = (uint32_t)(PADDR(txd_arr)); // Indicates start of descriptor ring buffer
+    *(uint32_t *)(e1000addr+E1000_TDBAH) = 0; // Make sure high bits are set to 0
+    *(uint16_t *)(e1000addr+E1000_TDLEN) = (uint16_t)(sizeof(struct e1000_tx_desc)*E1000_TXDARR_LEN); // Indicates length of descriptor ring buffer
     *(uint32_t *)(e1000addr+E1000_TDH) = 0;
     *(uint32_t *)(e1000addr+E1000_TDT) = 0;
     *(uint32_t *)(e1000addr+E1000_TCTL) = (E1000_TCTL_EN | E1000_TCTL_PSP | E1000_TCTL_CT | E1000_TCTL_COLD);
     *(uint16_t *)(e1000addr+E1000_TIPG) = (uint16_t)(E1000_TIPG_IPGT | E1000_TIPG_IPGR1 | E1000_TIPG_IPGR2);
+
+    // Initialize CMD bits for transmit descriptors
+    for (int i = 0; i < E1000_TXDARR_LEN; i++){
+        _reset_tdr(i);
+        txd_arr[i].status = E1000_TXD_STAT_DD;
+    }
+    return 0;
+}
+
+int E1000_transmit(void * data_addr, uint16_t length)
+{
+    cprintf("Call to E1000_transmit\n");
+    static uint32_t nextindex = 0;
+    struct e1000_tx_desc *nextdesc = (&txd_arr[nextindex]);
+    cprintf("Loc of txd_buff %d: 0x%x\n", nextindex, (uint32_t)(txd_arr[nextindex].buffer_addr));
+
+    if (!(nextdesc->status & E1000_TXD_STAT_DD))
+        return -1; // TODO: Error code; no free descriptors
+
+    if (length > E1000_ETH_PACKET_LEN)
+        length = E1000_ETH_PACKET_LEN;
+
+    _reset_tdr(nextindex);  // Reset this TDR
+    nextdesc->length = length;
+    memcpy((txd_bufs+nextindex), data_addr, length);
+    cprintf("Printing data: [");
+    uint8_t *data = (uint8_t *)(txd_bufs+nextindex);
+    for (int i = 0; i<length-1; i++)
+        cprintf("%d, ", data[i]);
+    cprintf("%d]\n", data[length-1]);
+
+    nextindex = (nextindex+1)%E1000_TXDARR_LEN;
+    *(uint32_t *)(e1000addr+E1000_TDT) = nextindex;
+    cprintf("End call to E1000_transmit\n");
     return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -2,7 +2,11 @@
 #define JOS_KERN_E1000_H
 #include <kern/pci.h>
 
+// Kernel functions
 int attach_E1000(struct pci_func *pcif);
+int E1000_transmit(void * data_addr, uint16_t length);
+
+#define E1000_TXDARR_LEN     32 /* Length of the transmit descriptor ring */
 
 /* Transmit Descriptor: sourced from e1000_hw.h */
 struct e1000_tx_desc {
@@ -15,9 +19,27 @@ struct e1000_tx_desc {
     uint16_t special;
 };
 
-#define E1000_TXDARR_LEN     32 /* Length of the transmit descriptor ring */
 /* Transmit Descriptor bit definitions */
-#define E1000_TXD_CMD_DEXT   0x20000000 /* Descriptor extension (0 = legacy) */
+#define E1000_TXD_DTYP_D     0x00100000         /* Data Descriptor */
+#define E1000_TXD_DTYP_C     0x00000000         /* Context Descriptor */
+#define E1000_TXD_POPTS_IXSM (uint8_t)0x01      /* Insert IP checksum */
+#define E1000_TXD_POPTS_TXSM (uint8_t)0x02      /* Insert TCP/UDP checksum */
+#define E1000_TXD_CMD_EOP    (uint8_t)0x01      /* End of Packet */
+#define E1000_TXD_CMD_IFCS   (uint8_t 0x02      /* Insert FCS (Ethernet CRC) */
+#define E1000_TXD_CMD_IC     (uint8_t)0x04      /* Insert Checksum */
+#define E1000_TXD_CMD_RS     (uint8_t)0x08      /* Report Status */
+#define E1000_TXD_CMD_RPS    (uint8_t)0x10      /* Report Packet Sent */
+#define E1000_TXD_CMD_DEXT   (uint8_t)0x20      /* Descriptor extension (0 = legacy) */
+#define E1000_TXD_CMD_VLE    (uint8_t)0x40      /* Add VLAN tag */
+#define E1000_TXD_CMD_IDE    (uint8_t)0x80      /* Enable Tidv register */
+#define E1000_TXD_STAT_DD    (uint8_t)0x01      /* Descriptor Done */
+#define E1000_TXD_STAT_EC    (uint8_t)0x02      /* Excess Collisions */
+#define E1000_TXD_STAT_LC    (uint8_t)0x04      /* Late Collisions */
+#define E1000_TXD_STAT_TC    (uint8_t)0x04      /* Tx Underrun */
+#define E1000_TXD_STAT_TU    (uint8_t)0x08      /* Transmit underrun */
+#define E1000_TXD_CMD_TCP    (uint8_t)0x01      /* TCP packet */
+#define E1000_TXD_CMD_IP     (uint8_t)0x02      /* IP packet */
+#define E1000_TXD_CMD_TSE    (uint8_t)0x04      /* TCP Seg enable */
 
 /* Register Set. (82543, 82544)
  *
@@ -35,6 +57,7 @@ struct e1000_tx_desc {
 #define E1000_TCTL     0x00400  /* TX Control - RW */
 #define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
 #define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    0x03804
 #define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
 #define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
 #define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
@@ -43,12 +66,15 @@ struct e1000_tx_desc {
 #define E1000_TCTL_EN     0x00000002    /* enable tx */
 #define E1000_TCTL_PSP    0x00000008    /* pad short packets */
 #define E1000_TCTL_CT     0x00000100    /* collision threshold */
-#define E1000_TCTL_COLD   0x00040000    /* collision threshold */
+#define E1000_TCTL_COLD   0x00040000    /* collision distance */
 
 /* Transmit IPG Register */
 #define E1000_TIPG_IPGT   10
 #define E1000_TIPG_IPGR1  8<<10
 #define E1000_TIPG_IPGR2  6<<20
 
+/* Ethernet Specifications */
+#define E1000_ETH_PACKET_LEN 1518
+typedef uint8_t packet_t[E1000_ETH_PACKET_LEN];
 
 #endif	// JOS_KERN_E1000_H
